0397726e5ce07e91c85687209ade5f04c24abd9f
SomeBottle <583797659@qq.com>
2018-05-20 14:42:00 +0800

    修复乱码问题

--- m.php
+++ m.php
@@ -1,3 +1,7 @@
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=2.0, user-scalable=yes" /> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
 <?php
 $d=$_GET['t'];
 if(empty($d)){
